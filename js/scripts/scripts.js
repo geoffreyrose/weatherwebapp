2028d252c88ec10c215b6c3c178747a3e7153aeb
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-07-08 13:37:43 -0700

    add abilty to change location

--- js/scripts/scripts.js
+++ js/scripts/scripts.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
-	$('button').click(function(){
+	$('.changeloc').hide();
+	$('#button').click(function(){
 		var zip = $('input').val();
-
+		$('.enterzip').hide();
+		$('#weather').show();
 		
-	$.simpleWeather({
-		zipcode: zip,
-		unit: 'f',
-		success: function(weather) {
-		  html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-		  html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-		  html += '<li class="currently">'+weather.currently+'</li>';
-		  html += '<li>'+weather.tempAlt+'&deg;c</li></ul>';
-	  
-		  $("#weather").html(html);
-		},
-		
-		error: function(error) {
-		  $("#weather").html('<p>'+error+'</p>');
-		  }
+		$.simpleWeather({
+			zipcode: zip,
+			unit: 'f',
+			success: function(weather) {
+			  html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+			  html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+			  html += '<li class="currently">'+weather.currently+'</li>';
+			  //html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
+		  
+			  $("#weather").html(html);
+			},
+			
+			error: function(error) {
+			  $("#weather").html('<p>'+error+'</p>');
+			  }
+		});
+		$('.changeloc').show();
 	});
+	
+	$('.changeloc').click(function(){
+		$('#weather').hide();
+		$('.enterzip').show();
+		$('.changeloc').hide();
+	});
+	
 });
-	});
