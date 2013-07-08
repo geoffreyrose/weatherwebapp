e6ff103619f4f4e6ec0348f9f9e0bdb5529dfcc8
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-07-08 14:26:32 -0700

    Forcasted high/low + basic styling

--- js/scripts/scripts.js
+++ js/scripts/scripts.js
@@ -9,17 +9,21 @@ $(document).ready(function() {
 			zipcode: zip,
 			unit: 'f',
 			success: function(weather) {
-			  html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-			  html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-			  html += '<li class="currently">'+weather.currently+'</li>';
-			  //html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
-		  
-			  $("#weather").html(html);
+				html = '<h1>'+weather.city+', '+weather.region+'</h1>';
+				html += '<img src="'+weather.image+'" />';
+				html += '<h2>'+"Current Temperature"+'</h2>';
+				html += '<ul><li>'+weather.temp+'&deg;'+weather.units.temp+'</li>';
+				html += '<li>'+weather.tempAlt+'&deg;C</li>';
+				html += '<h3>'+"Today's Forcast"+'</h3>';
+				html += '<li class="high">'+"High "+weather.high+'</li>'+'<li class="low">'+"Low "+weather.low+'</li></ul>';
+				//html += '<h3 class="currently">'+weather.currently+'</h3>';
+				
+				$("#weather").html(html);
 			},
 			
 			error: function(error) {
-			  $("#weather").html('<p>'+error+'</p>');
-			  }
+				$("#weather").html('<p>'+error+'</p>');
+				}
 		});
 		$('.changeloc').show();
 	});
