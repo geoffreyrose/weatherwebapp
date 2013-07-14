6b9fd546a72f183e9fef4803f781069c811908e0
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-07-14 00:14:21 -0700

    can now choose location with city, state

--- js/scripts/scripts.js
+++ js/scripts/scripts.js
@@ -5,22 +5,24 @@ if ('standalone' in navigator && !navigator.standalone && (/iphone|ipod|ipad/gi)
 }
 	$('.changeloc').hide();
 	$('#button').click(function(){
-		var zip = $('input').val();
+		var zip = $('input.zip').val();
+		var cLoc = $('input.cloc').val();
 		$('.enterzip').hide();
 		$('#weather').show();
 		
 		$.simpleWeather({
 			zipcode: zip,
 			unit: 'f',
+			location: cLoc,
 			success: function(weather) {
 				html = '<h1>'+weather.city+', '+weather.region+'</h1>';
 				html += '<p>'+'<span class="weather-'+weather.code+'" >'+'</span>'+'</p>';
 				html += '<ul><li>'+weather.temp+'&deg;'+weather.units.temp+'</li>';
 				html += '<li>'+weather.currently+'</li>';
 				html += '<li class="high">'+"High "+weather.high+'&deg;F'+'</li>'+'<li class="low">'+"Low "+weather.low+'&deg;F'+'</li></ul>';
-				//html += '<h3 class="currently">'+weather.currently+'</h3>';
 				
 				$("#weather").html(html);
+				$("#weather2").html(html);
 			},
 			
 			error: function(error) {
