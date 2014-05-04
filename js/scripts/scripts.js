6fb32baaee6bf43acad409b3d61a4f8a302f5104
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2014-05-04 03:17:49 -0700

    rewrote with post get data so your can save url

--- js/scripts/scripts.js
+++ js/scripts/scripts.js
@@ -1,71 +1,64 @@
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
 $(document).ready(function() {
+	var zip = urlParams.zip;
+	var weatherLocation = urlParams.city;
 
 
-if ('standalone' in navigator && !navigator.standalone && (/iphone|ipod|ipad/gi).test(navigator.platform) && (/Safari/i).test(navigator.appVersion)) {
-   	$('p.ios').show();
-}
-	$('.changeloc').hide();
-	$('#button').click(function(){
-		var zip = $('input.zip').val();
-		var cLoc = $('input.cloc').val();
-		$('.enterzip').hide();
-		$('#weather').show();
-		
+	if ('standalone' in navigator && !navigator.standalone && (/iphone|ipod|ipad/gi).test(navigator.platform) && (/Safari/i).test(navigator.appVersion)) {
+	   	$('p.ios').show();
+	}
+
+	if( zip || weatherLocation ) {
+		$('.select-location').hide();
+		$('.weather, .weather-controls').show();
+
+
 		$.simpleWeather({
 			zipcode: zip,
 			unit: 'f',
-			location: cLoc,
-			woeid: cLoc,
+			location: weatherLocation,
+			woeid: weatherLocation,
 			success: function(weather) {
 				html = '<h1>'+weather.city+', '+weather.region+'</h1>';
-				html += '<p>'+'<span class="weather-'+weather.code+'" >'+'</span>'+'</p>';
-				html += '<ul>'+'<li class="f-temp">'+weather.temp+'&deg;'+weather.units.temp+'</li>'+'<li class="c-temp cC">'+weather.tempAlt+'&deg;'+weather.units.tempAlt+'</li>';
-				html += '<li>'+weather.currently+'</li></ul>';
-				html += '<hr />';
-				html += '<ul class="f-temp weatherforcast"><li>'+"Today"+'</li>'+'<li class="f-temp">'+"High "+weather.high+'&deg;F'+'</li>'+'<li class="f-temp">'+"Low "+weather.low+'&deg;F'+'</li></ul>';
-				html += '<ul class="f-temp weatherforcast"><li>'+"Tomorrow"+'</li>'+'<li class="f-temp">'+"High "+weather.tomorrow.high+'&deg;F'+'</li>'+'<li class="f-temp">'+"Low "+weather.tomorrow.low+'&deg;F'+'</li></ul>';
+				html += '<p class="weather-icon"><span class="weather-'+weather.code+'"></span></p>';
+				html += '<h2 class="current-weather">'+weather.currently+'<span class="fahrenheit"> '+weather.temp+'&deg;F</span><span class="celsius"> '+weather.tempAlt+'&deg;C</span></h2>';
 
-				html += '<ul class="weatherforcast c-temp"><li>'+"Today"+'</li>'+'<li class="c-temp">'+"High "+weather.highAlt+'&deg;C'+'</li>'+'<li class="c-temp">'+"Low "+weather.lowAlt+'&deg;C'+'</li></ul>';
-				html += '<ul class="weatherforcast c-temp"><li>'+"Tomorrow"+'</li>'+'<li class="c-temp">'+"High "+weather.tomorrow.highAlt+'&deg;C'+'</li>'+'<li class="c-temp">'+"Low "+weather.tomorrow.lowAlt+'&deg;C'+'</li></ul>';
+				//Fahrenheit  
+				html += '<section class="forcast fahrenheit"><h3>Today</h3><ul class="weatherforcast"><li>High '+weather.high+'&deg;F</li><li>Low '+weather.low+'&deg;F</li></ul>';
+				html += '<h3>Tomorrow</h3><ul class="weatherforcast"><li>High '+weather.tomorrow.high+'&deg;F</li><li>Low '+weather.tomorrow.low+'&deg;F</li></ul></section>';
 
-				html += '<hr />';
-				html += '<ul class="sun"><li>'+"Sunrise"+'</li>'+'<li>'+weather.sunrise+'</li>';
-				html += '<li>'+"Sunset"+'</li>'+'<li>'+weather.sunset+'</li></ul>';
-				html += '<hr />';
+				//Celsius    
+				html += '<section class="forcast celsius"><h3>Today</h3><ul class="weatherforcast"><li>High '+weather.highAlt+'&deg;C</li><li>Low '+weather.lowAlt+'&deg;C</li></ul>';
+				html += '<h3>Tomorrow</h3><ul class="weatherforcast"><li>High '+weather.tomorrow.highAlt+'&deg;C</li><li>Low '+weather.tomorrow.lowAlt+'&deg;C</li></ul></section>';
+
+				//sunrise
+				html += '<section class="sunrise"><ul><li><h3>Sunrise</h3></li><li class="time">'+weather.sunrise+'</li></ul><ul><li><h3>Sunset</h3></li><li class="time">'+weather.sunset+'</li></ul></section>';
 				
-				$("#weather").html(html);
-				$("#weather2").html(html);
+				$('.weather').html(html);
 			},
 			
 			error: function(error) {
-				$("#weather").html('<p>'+error+'</p>');
-				}
+				$('.weather').html('<p>'+error+'</p>');
+			}
 		});
-		$('.changeloc').show();
-		$('.c-f').show();
-		$('.c-deg').removeClass('current-deg');
-		$('.f-deg').addClass('current-deg');
-	});
-	
-	$('.changeloc').click(function(){
-		$('#weather').hide();
-		$('.enterzip').show();
-		$('.changeloc').hide();
-		$('.c-f').hide();
+	}
+
+	$('.weather-controls .change').on('click', function(){
+		$('.select-location').show();
+		$('.weather, .weather-controls').hide();
 	});
 
-	$('.c-deg').click(function(){
-		$('.f-temp').hide();
-		$('.f-deg').removeClass('current-deg');
-		$(this).addClass('current-deg');
-		$('.c-temp').show();
-	});
-
-	$('.f-deg').click(function(){
-		$('.c-temp').hide();
-		$('.c-deg').removeClass('current-deg');
-		$(this).addClass('current-deg');
-		$('.f-temp').show();
-	});
 	
 });
